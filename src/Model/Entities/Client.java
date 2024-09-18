5dcdb9231861d0161ccce5ca20839f7579bbd932
Sahtani <soumia.sahtani@gmail.com>
2024-09-18 15:13:02 +0100

    Add Project entity with getters and setters

---
+++ src/Model/Entities/Client.java
@@ -0,0 +1,80 @@
+package Model.Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+    private boolean isProfessional;
+    private List<Project> projects;
+
+    // Constructor
+    public Client(String name, String address, String phone, boolean isProfessional) {
+
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.isProfessional = isProfessional;
+        this.projects = new ArrayList<>();
+    }
+
+    public Client() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isProfessional() {
+        return isProfessional;
+    }
+
+    public void setProfessional(boolean professional) {
+        isProfessional = professional;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "name='" + name + '\'' + ", address='" + address + '\'' + ", phone='" + phone + '\'' + ", isProfessional=" + isProfessional + ", projects=" + projects + '}';
+    }
+}
---
+++ src/Model/Entities/Project.java
@@ -0,0 +1,79 @@
+package Model.Entities;
+
+import Model.Enums.ProjectStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project {
+
+    private String projectName;
+    private double profitMargin;
+    private double totalCost;
+    private ProjectStatus status;
+    private Client client ;
+//    private List<Component> components;
+//    private Estimate estimate ;
+
+
+    // Constructor
+    public Project(String projectName, double profitMargin, double totalCost, ProjectStatus status, Client client) {
+        this.projectName = projectName;
+        this.profitMargin = profitMargin;
+        this.totalCost = totalCost;
+        this.status = status;
+        this.client = client ;
+//        this.components = new ArrayList<>();
+    }
+    public Project(){
+
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public double getProfitMargin() {
+        return profitMargin;
+    }
+
+    public void setProfitMargin(double profitMargin) {
+        this.profitMargin = profitMargin;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public ProjectStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ProjectStatus status) {
+        this.status = status;
+    }
+
+    public Client getClient(){
+        return client ;
+    }
+    public void setClient(Client client){
+        this.client = client ;
+    }
+
+//    public List<Component> getComponents() {
+//        return components;
+//    }
+//
+//    public void setComponents(List<Component> components) {
+//        this.components = components;
+//    }
+
+}
