d88d2810e364f71fad09eb45fd07912a4bab8160
Sahtani <soumia.sahtani@gmail.com>
2024-09-19 09:49:59 +0100

    Added new entities: Component, Labor, and Material

---
+++ src/Model/Entities/Component.java
@@ -0,0 +1,51 @@
+package Model.Entities;
+
+public abstract class Component {
+
+    protected String name;
+    protected String componentType;
+    protected double vatRate;
+    protected Project project ;
+
+    // Constructor
+    public Component(String name,String componentType, double vatRate ) {
+        this.name = name;
+        this.componentType = componentType;
+        this.vatRate = vatRate;
+
+    }
+
+    public Component(){
+
+    }
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComponentType() {
+        return componentType;
+    }
+
+    public void setComponentType(String componentType) {
+        this.componentType = componentType;
+    }
+
+    public double getVatRate() {
+        return vatRate;
+    }
+
+    public void setVatRate(double vatRate) {
+        this.vatRate = vatRate;
+    }
+    public Project getProject(){
+        return project ;
+    }
+    public void setProject(Project project){
+        this.project = project ;
+    }
+}
